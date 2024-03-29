e2effcd00bdf422515e6610ed22906004410217e
ranyereramires <ranyereramires@gmail.com>
2024-03-29 11:07:42 -0300

    criado const de configuração da geração de cada tipo de sorteio

---
+++ src/app/models/LotteriesDetailConst.ts
@@ -0,0 +1,83 @@
+export const LotteriesDetail = {
+    maismilionaria : {
+        minimoNumeros:6,
+        maximoNumeros:12,
+        menorNumero:1,
+        maiorNumero:50,
+        geraMes:false,
+        geraTime:false,
+        regraMultipla:1
+    },
+    megasena : {
+        minimoNumeros:6,
+        maximoNumeros:15,
+        menorNumero:1,
+        maiorNumero:60,
+        geraMes:false,
+        geraTime:false,
+        regraMultipla:1
+    },
+    lotofacil : {
+        minimoNumeros:5,
+        maximoNumeros:15,
+        menorNumero:1,
+        maiorNumero:25,
+        geraMes:false,
+        geraTime:false,
+        regraMultipla:1
+    },
+    quina : {
+        minimoNumeros:5,
+        maximoNumeros:15,
+        menorNumero:1,
+        maiorNumero:80,
+        geraMes:false,
+        geraTime:false,
+        regraMultipla:1
+    },
+    lotomania : {
+        minimoNumeros:0,
+        maximoNumeros:99,
+        menorNumero:1,
+        maiorNumero:100,
+        geraMes:false,
+        geraTime:false,
+        regraMultipla:1
+    },
+    timemania : {
+        minimoNumeros:10,
+        maximoNumeros:10,
+        menorNumero:1,
+        maiorNumero:80,
+        geraMes:false,
+        geraTime:true,
+        regraMultipla:1
+    },
+    duplasena : {
+        minimoNumeros:6,
+        maximoNumeros:15,
+        menorNumero:1,
+        maiorNumero:50,
+        geraMes:false,
+        geraTime:true,
+        regraMultipla:1
+    },
+    diadesorte : {
+        minimoNumeros:7,
+        maximoNumeros:15,
+        menorNumero:1,
+        maiorNumero:31,
+        geraMes:true,
+        geraTime:false,
+        regraMultipla:1
+    },
+    supersete : {
+        minimoNumeros:1,
+        maximoNumeros:1,
+        menorNumero:1,
+        maiorNumero:9,
+        geraMes:true,
+        geraTime:true,
+        regraMultipla:7
+    }
+}
--- src/app/pages/lotteries/generate-numbers/generate-numbers.component.ts
+++ src/app/pages/lotteries/generate-numbers/generate-numbers.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../../services/state.service';
+import { LotteriesResultService } from '../../../services/lotteries-result.service';
+import { ShowToastrService } from '../../../services/show-toastr.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-generate-numbers',
   templateUrl: './generate-numbers.component.html',
   styleUrl: './generate-numbers.component.scss'
 })
-export class GenerateNumbersComponent {
+export class GenerateNumbersComponent implements OnInit{
+
+  lotteries:string[] = [];
+
+  constructor(
+    private stateService:StateService,
+    private lotteriesResulService:LotteriesResultService,
+    private showToastrService:ShowToastrService,
+    private formBuilder:FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllLotteries();
+  }  
+
+
+  getAllLotteries(){
+    this.stateService.setIsLoading(true);
+    this.lotteriesResulService.getAllTypesLotteries().subscribe({
+      next: (data) => {
+        var removeFederal = data.filter(x => x != 'federal')
+        this.lotteries = removeFederal;
+        this.stateService.setIsLoading(false);
+      },
+      error: (e) => {
+        this.showToastrService.showError('Houve um erro ao buscar as loterias');
+        this.stateService.setIsLoading(false);
+      }
+    })
+  }
 
 }
