5c382f6a937d1473f0a768826113c20739e2f6f6
Ranyere <ranyereramiresg@hotmail.com>
2024-09-27 12:41:47 -0300

    novo ajuste, correção finalizada

--- src/app/pages/lotteries/generate-numbers/generate-numbers.component.ts
+++ src/app/pages/lotteries/generate-numbers/generate-numbers.component.ts
@@ -182,34 +182,4 @@ generateShamrocks(quantity:number) : number[]{
   return numeros;
 }
 
-
-transformTitle(nameLotterie:string) {
-
-    let name = "";
-    if (nameLotterie === "maismilionaria") {
-      name = "Mais Milionária";
-  } else if (nameLotterie === "megasena") {
-      name = "Mega Sena";
-  } else if (nameLotterie === "lotofacil") {
-      name = "Lotofácil";
-  } else if (nameLotterie === "quina") {
-      name = "Quina";
-  } else if (nameLotterie === "lotomania") {
-      name = "Loto Mania";
-  } else if (nameLotterie === "timemania") {
-      name = "Time Mania";
-  } else if (nameLotterie === "duplasena") {
-      name = "Dupla Sena";
-  } else if (nameLotterie === "federal") {
-      name = "Federal";
-  } else if (nameLotterie === "diadesorte") {
-      name = "Dia de Sorte";
-  } else if (nameLotterie === "supersete") {
-      name = "Super Sete";
-  } else {
-      name = "Loteria não encontrada";
-  }
-    return name;
-  }
-
 }
